1c6233b2f97f4e2135ce0b988d903a0233c1b61f
Thomas Lupin <thomas.lupin@epita.fr>
2019-10-02 00:03:42 +0200

    app: fix timestamp check

--- app.js
+++ app.js
@@ -18,13 +18,18 @@ async function monitoring() {
             fb_login.generateAppState(() => monitoring());
             return;
         }
+        api.setOptions({
+            selfListen: true,
+            logLevel: "silent"
+        });
         console.log("Start monitoring !");
         api.listen((err, message) => {
             console.log(message);
             if (message.threadID == '2175128779192067') {
-                let date = new Date(message.timestamp);
+                let date = new Date(int(message.timestamp));
                 if (date.getMinutes() == date.getHours()) {
-                    api.setMessageReaction(":love:", message.messageID);
+                    console.log("C'est l'heure !!");
+                    api.setMessageReaction(":love:", message.messageID, (err) => { if (err) console.error(err) });
                 }
             }
 
