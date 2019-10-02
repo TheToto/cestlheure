c871d41a840bf46f4f7a84713e84045ad0b0f3d8
Thomas Lupin <thomas.lupin@epita.fr>
2019-10-03 01:14:19 +0200

    db: add some functions to dump participants, correct hours, etc

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 const fb_login = require('./login');
 const db = require('./db');
 
-async function monitoring() {
+async function action(callback) {
     fb_login.connectAppState((api) => {
         api.setOptions({
             selfListen: true,
             logLevel: "silent"
         });
+        callback(api);
+    });
+}
+
+async function monitoring() {
+    action((api) => {
         console.log("Start monitoring !");
         api.listen((err, message) => {
             console.log(message);
+            db.saveMessages([message]);
             if (message.threadID == '2175128779192067') {
-                let date = new Date(int(message.timestamp));
+                let date = new Date(parseInt(message.timestamp));
                 if (date.getMinutes() == date.getHours()) {
                     console.log("C'est l'heure !!");
                     api.setMessageReaction(":love:", message.messageID, (err) => {
@@ -23,15 +30,43 @@ async function monitoring() {
     });
 }
 
-async function dump_fb() {
-    fb_login.connectAppState((api) => {
-        api.setOptions({
-            selfListen: true,
-            logLevel: "silent"
-        });
+async function dump_thread() {
+    action((api) => {
         db.saveAll(api, undefined);
     });
 }
 
-//monitoring();
-dump_fb();
+async function dump_users() {
+    get_list_user((list) => {
+        db.dump_participants(list);
+    });
+}
+
+async function info_thread(callback) {
+    action((api) => {
+        api.getThreadInfo('2175128779192067', function (err, info) {
+            if (err) return console.error(err);
+            callback(info, api);
+        });
+    });
+}
+
+async function get_list_user(callback) {
+    info_thread((info, api) => {
+        api.getUserInfo(info.participantIDs, (err, ret) => {
+            if (err) return console.error(err);
+            let list = [];
+            for (let user in ret) {
+                let obj = ret[user];
+                obj._id = user;
+                list.push(obj);
+            }
+            callback(list);
+        });
+    });
+}
+
+monitoring();
+//dump_thread();
+//dump_users();
+//db.dump_heure();
--- db.js
+++ db.js
@@ -1,6 +1,16 @@
 require('dotenv').config();
 const MongoClient = require('mongodb').MongoClient;
 
+function connect(callback) {
+    MongoClient.connect(process.env.MONGODB_URI, function (err, db) {
+        if (err) throw err;
+        let dbo = db.db("heroku_2z8057dw");
+        callback(dbo, function () {
+            db.close();
+        });
+    });
+}
+
 function saveAll(api, timestamp) {
     console.log("Load time : " + timestamp);
     api.getThreadHistory('2175128779192067', 20, timestamp, (err, history) => {
@@ -17,24 +27,81 @@ function saveAll(api, timestamp) {
     });
 }
 
+function oprations_from_list(list) {
+    let operations = [];
+    for (let i in list) {
+        operations.push({
+            updateOne: {
+                "filter": {
+                    "_id": list[i]._id
+                },
+                "update": list[i],
+                "upsert": true
+            }
+        });
+    }
+    return operations;
+}
+
 function saveMessages(messages) {
     for (let i in messages) {
         messages[i]._id = messages[i].messageID;
     }
-    MongoClient.connect(process.env.MONGODB_URI, function (err, db) {
-        if (err) throw err;
-        let dbo = db.db("heroku_2z8057dw");
-        dbo.collection("messages").insertMany(messages, {
-            ordered: false
-        }, function (err, res) {
-            if (err) console.log("dup was found ?");;
-            console.log("Inserted !");
-            db.close();
+    connect(function (dbo, end) {
+        dbo.collection("messages").bulkWrite(oprations_from_list(messages));
+        console.log("Inserted !");
+        end();
+    });
+}
+
+async function dump_participants(id_list) {
+    connect(function (dbo, end) {
+        dbo.collection("participants").bulkWrite(oprations_from_list(id_list));
+        console.log("Dump done.");
+        end();
+    });
+}
+
+function isSameMin(d1, d2) {
+    return d1.getDate() === d2.getDate() &&
+        d1.getMonth() === d2.getMonth() &&
+        d1.getFullYear() === d2.getFullYear() &&
+        d1.getHours() == d2.getHours() &&
+        d1.getMinutes() == d2.getMinutes();
+}
+
+function dateWithoutSeconds(date) {
+    return new Date(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), date.getMinutes(), 0, 0);
+}
+
+async function dump_heure() {
+    let last_heure = new Date(1970, 1, 1, 0, 0, 0, 0);
+    let correct_hours = [];
+    connect(function (dbo, end) {
+        dbo.collection("messages").find().sort({
+            timestamp: 1
+        }).toArray(function (err, result) {
+            if (err) throw err;
+            for (let i in result) {
+                let time = new Date(parseInt(result[i].timestamp));
+                if (!isSameMin(last_heure, time) && time.getHours() == time.getMinutes()) {
+                    let obj = result[i];
+                    obj._id = dateWithoutSeconds(time);
+                    correct_hours.push(obj);
+                    last_heure = time;
+                }
+            }
+            dbo.collection("cestlheure").bulkWrite(oprations_from_list(correct_hours));
+            console.log("Dump done.");
+            end();
         });
     });
+
 }
 
 module.exports = {
     saveAll,
-    saveMessages
+    saveMessages,
+    dump_participants,
+    dump_heure
 };
