147bd30d974248eaa5b3f039069feee1cc3e1279
Thomas Lupin <thomas.lupin@epita.fr>
2019-10-04 11:31:24 +0200

    twig: setup twing html template

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Twig = require('twig');
 const fb = require('./fb');
 const db = require('./db');
 
@@ -6,8 +7,9 @@ let app = express();
 let PORT = 1234;
 
 app.get('/', function (req, res) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.send('Vous êtes à l\'accueil');
+    res.render('index.twig', {
+        message: "Hello World"
+    });
 });
 
 app.listen(PORT);
@@ -15,6 +17,7 @@ console.log("App listening on port " + PORT);
 fb.monitoring();
 //fb.dump_users().then(() => console.log("Dump user done"));
 /*fb.dump_thread().then(() => {
-    console.log("Dump thread done")
+    console.log("Dump thread done");
     db.fix_heure().then(() => console.log("Fix heure done"));
 });*/
+//db.fix_heure().then(() => console.log("Fix heure done"));
