3f203bf07f72b796731a79b5205350694b0340cf
Thomas Lupin <thomas.lupin@epita.fr>
2019-10-05 18:20:15 +0200

    db_fetch: getMonthScore

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
 const express = require('express');
 const Twig = require('twig');
 const fb = require('./fb');
+const db = require('./db');
 const db_fetch = require('./db_fetch');
 
 let app = express();
 let PORT = 1234;
 
 app.get('/', function (req, res) {
-    db_fetch.getData(db_fetch.getGlobalScore).then((arr) => {
+    db_fetch.getData([db_fetch.getGlobalScore, db_fetch.getCurrentMonthScore]).then((arr) => {
         res.render('index.twig', {
-            data: arr
+            global: arr[0],
+            month: arr[1]
         });
     }).catch((err) => res.send(err));
 });
 
 app.listen(PORT);
 console.log("App listening on port " + PORT);
-fb.monitoring();
+//fb.monitoring();
 //fb.dump_users().then(() => console.log("Dump user done"));
 /*fb.dump_thread().then(() => {
     console.log("Dump thread done");
--- db_fetch.js
+++ db_fetch.js
@@ -31,6 +31,48 @@ async function getGlobalScore(dbo) {
     });
 }
 
+async function getMonthScore(dbo, year, month) {
+    let curMonth = new Date(year, month, 0, 0, 0, 0, 0);
+    let nextMonth = new Date(year, month + 1, 0, 0, 0, 0, 0);
+    return new Promise((resolve, reject) => {
+        dbo.collection("messages").aggregate([{
+            $match: {
+                'cestlheure': 1,
+                'timestamp': {
+                    $gte: curMonth.getTime(),
+                    $lt: nextMonth.getTime()
+                }
+            }
+        }, {
+            $group: {
+                _id: "$senderID",
+                count: {
+                    $sum: 1
+                }
+            }
+        }, {
+            $lookup: {
+                from: "participants",
+                localField: "_id",
+                foreignField: "_id",
+                as: "sender"
+            }
+        }, {
+            $sort: {
+                count: -1
+            }
+        }]).toArray((err, arr) => {
+            if (err) return reject(err);
+            resolve(arr);
+        });
+    });
+}
+
+async function getCurrentMonthScore(dbo) {
+    let date = new Date();
+    return getMonthScore(dbo, date.getFullYear(), date.getMonth());
+}
+
 async function getUsers(dbo) {
     return new Promise((resolve, reject) => {
         dbo.collection("partitipants").find().toArray((err, arr) => {
@@ -43,8 +85,14 @@ async function getUsers(dbo) {
 async function getData(getter) {
     return new Promise((resolve, reject) => {
         db.connect().then(([dbo, db]) => {
-            getter(dbo).then((arr) => {
-                resolve(arr);
+            console.log(getter);
+            let promises = [];
+            for (let get in getter) {
+                promises.push(getter[get](dbo));
+            }
+            Promise.all(promises).then((results) => {
+                console.log(results);
+                resolve(results);
                 db.close();
             }).catch(reject);
         }).catch(reject);
@@ -54,5 +102,7 @@ async function getData(getter) {
 module.exports = {
     getData,
     getGlobalScore,
-    getUsers
+    getUsers,
+    getMonthScore,
+    getCurrentMonthScore
 };
