4b14831f39f5f56daac223943cc91c1f5dcef47e
Thomas Lupin <thomas.lupin@epita.fr>
2020-01-01 18:42:02 +0100

    colorfield: add colorfield to choose color in admin

--- fbbot/models.py
+++ fbbot/models.py
@@ -1,15 +1,14 @@
 from django.db import models
 from django.contrib.postgres.fields import JSONField
-from django.core.serializers.json import DjangoJSONEncoder
-
+from colorfield.fields import ColorField
 
 class User(models.Model):
     uid = models.CharField(primary_key=True, max_length=50)
     name = models.CharField(max_length=100)
-    nickname = models.CharField(max_length=50, null=True)
-    url = models.URLField(max_length=400, null=True)
-    photo_url = models.URLField(max_length=400, null=True)
-    color = models.CharField(max_length=20, null=True)
+    nickname = models.CharField(max_length=50, null=True, blank=True)
+    url = models.URLField(max_length=400, null=True, blank=True)
+    photo_url = models.URLField(max_length=400, null=True, blank=True)
+    color = ColorField(default='#000000')
 
     def __str__(self):
         return self.name
@@ -17,10 +16,10 @@ class User(models.Model):
 
 class Message(models.Model):
     uid = models.CharField(primary_key=True, max_length=50)
-    text = models.TextField(null=True)
+    text = models.TextField(null=True, blank=True)
     author = models.ForeignKey('User', on_delete=models.DO_NOTHING)
     time = models.DateTimeField()
-    full_object = JSONField(null=True)
+    full_object = JSONField(null=True, blank=True)
 
     def __str__(self):
         body = self.text[:15] if self.text else "None"
