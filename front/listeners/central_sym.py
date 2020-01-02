e93311a86e510b88b0ef9ecf8bc1b21cf44a61f1
Thomas Lupin <thomas.lupin@epita.fr>
2020-01-02 22:25:15 +0100

    listener: add 00h00 to central_sym.py

--- front/listeners/central_sym.py
+++ front/listeners/central_sym.py
@@ -4,5 +4,5 @@ from fbchat import Message
 
 class CentralSymListener(ListCheckListener):
     NAME = "central_sym"
-    LIST_HOURS = [(2, 50), (5, 20), (20, 5), (22, 55), (11, 11), (12, 51), (15, 21), (21, 15)]
+    LIST_HOURS = [(0, 0), (2, 50), (5, 20), (20, 5), (22, 55), (11, 11), (12, 51), (15, 21), (21, 15)]
     MESSAGE_CONTENT = "Symétrie centrale !"
