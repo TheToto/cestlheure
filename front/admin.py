a817ebc6d15342eaa901e0683f24caded7e5100f
Thomas Lupin <thomas.lupin@epita.fr>
2020-01-02 22:46:56 +0100

    admin: optimize admin edit page loading

--- front/admin.py
+++ front/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+
 from .models import CestLheure, CestLheureIndex
 
+
+class CestLheureAdmin(ModelAdmin):
+    raw_id_fields = ['message']
+
+
+class CestLheureIndexAdmin(ModelAdmin):
+    raw_id_fields = ['last_listened']
+
+
 # Register your models here.
-admin.site.register(CestLheure)
-admin.site.register(CestLheureIndex)
+admin.site.register(CestLheure, CestLheureAdmin)
+admin.site.register(CestLheureIndex, CestLheureIndexAdmin)
