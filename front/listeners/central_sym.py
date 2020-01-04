6312155977047ddfd739865675a6ebd9243656f1
Thomas Lupin <thomas.lupin@epita.fr>
2020-01-04 01:12:12 +0100

    listener: add 01:10 to central sym

--- front/listeners/central_sym.py
+++ front/listeners/central_sym.py
@@ -3,5 +3,5 @@ from .list_check import ListCheckListener
 
 class CentralSymListener(ListCheckListener):
     NAME = "central_sym"
-    LIST_HOURS = [(0, 0), (2, 50), (5, 20), (20, 5), (22, 55), (11, 11), (12, 51), (15, 21), (21, 15)]
+    LIST_HOURS = [(0, 0), (1,10), (2, 50), (5, 20), (20, 5), (22, 55), (11, 11), (12, 51), (15, 21), (21, 15)]
     MESSAGE_CONTENT = "Symétrie centrale !"
